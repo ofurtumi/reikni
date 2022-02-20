9d92d9d0d7a7232a1b9e96a17cf9fcb6827e41f6
Tumi <thorvaldur.tumi@gmail.com>
2022-02-20 10:49:30 +0000

    skiladæmi 5

--- test/test.py
+++ test/test.py
@@ -1,2 +1,21 @@
-import numpy as np
+def readWords():
+    ordlist = []
+    with open("./ord.txt",'r',encoding = 'utf-8') as f:
+        ordlist = f.read().split()
+        f.close()
+    return ordlist
 
+def lookfor(word):
+    if (len(word) != 4): return -1
+    if (word[0] == word[3] 
+    and word[1] == word[2]): return 1
+    return -1
+
+ord = readWords()
+
+abba = []
+for o in ord:
+    if (lookfor(o) == 1): 
+        abba.append(o)
+
+print(abba)
